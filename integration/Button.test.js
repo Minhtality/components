e29108855d573ac245560841650e6f5cc7165def
James Ives <iam@jamesiv.es>
2019-02-01 13:44:02 -0500

    one test

--- integration/Button.test.js
+++ integration/Button.test.js
@@ -1,5 +1,10 @@
 describe('Button', () => {
   it('primary light looks correct', async () => {
-    expect(true).toBe(true);
+    await page.goto(
+      'http://localhost:9009/iframe.html?selectedKind=Atoms%2FButton&selectedStory=primary%20light&full=0'
+    );
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
   });
+
 });
