a49739eafedd975767d9fa218e6831a05e6b1ade
James Ives <iam@jamesiv.es>
2019-02-01 13:46:25 -0500

    test

--- integration/Button.test.js
+++ integration/Button.test.js
@@ -1,10 +1,10 @@
 describe('Button', () => {
   it('primary light looks correct', async () => {
     await page.goto(
-      'http://localhost:9009/iframe.html?selectedKind=Atoms%2FButton&selectedStory=primary%20light&full=0'
+      'http://www.google.com'
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    await expect(image).toMatchImageSnapshot();
   });
 
 });
